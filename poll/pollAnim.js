d6f38488ab2f92344379bfeb371c7d3103f1cd81
Foxxite <foxxite@foxxite.com>
2020-09-10 20:49:35 +0200

    Added char selection section to animation

--- poll/pollAnim.js
+++ poll/pollAnim.js
@@ -10,6 +10,7 @@ let select1 = new Audio("../assets/audio/select_screen1.ogg");
 let select2 = new Audio("../assets/audio/select_screen2.ogg");
 let select3 = new Audio("../assets/audio/select_screen3.ogg");
 let select4 = new Audio("../assets/audio/select_screen4.ogg");
+let impactEffect = new Audio("../assets/audio/impact02.ogg");
 
 let awaitAudio = setInterval(function () {
 	if (backgroundMusic.readyState == 4) {
@@ -20,6 +21,7 @@ let awaitAudio = setInterval(function () {
 async function startMusic() {
 	try {
 		await backgroundMusic.play();
+		backgroundMusic.loop = true;
 		startAnims();
 	} catch (err) {
 		alert("Audio is used for timing, please allow auto play for audio and reload");
@@ -45,7 +47,7 @@ function startAnims() {
 		dingEffect.play();
 
 		pressStart();
-	}, 200);
+	}, 500);
 }
 
 function pressStart() {
@@ -58,7 +60,57 @@ function pressStart() {
 
 	setTimeout(function () {
 		select4.play();
+		flash();
 		start.style.display = "none";
 		players.style.display = "block";
-	}, 500 + 1750);
+
+		playerSelector();
+	}, 500 + 2000);
+}
+
+function playerSelector() {
+	let switchCount = 0;
+
+	let switchPlayer = setInterval(function () {
+		if (switchCount == 4) {
+			clearInterval(switchPlayer);
+
+			let title = document.getElementsByClassName("title")[0];
+			title.style.display = "none";
+
+			impactEffect.play();
+			flash();
+
+			let selection = document.getElementsByClassName("selection")[0];
+			selection.style.display = "block";
+		} else {
+			jumpEffect.play();
+
+			let selected = document.getElementsByClassName("selected")[0];
+			let unselected = document.getElementsByClassName("unselected")[0];
+
+			selected.classList.remove("selected");
+			selected.classList.add("unselected");
+
+			unselected.classList.remove("unselected");
+			unselected.classList.add("selected");
+
+			switchCount++;
+
+			if (switchCount == 4) {
+				let player1 = document.getElementById("player1");
+				player1.classList.add("blink");
+			}
+		}
+	}, 1200);
+}
+
+function flash() {
+	let flash = document.getElementsByClassName("flash")[0];
+
+	flash.style.display = "block";
+
+	setTimeout(function () {
+		flash.style.display = "none";
+	}, 100);
 }
